ce5ed56efff28e160c206c2565820ad6533173e9
Chris <c.lamothe25@gmail.com>
2018-04-23 08:33:41 -0400

    Starting work on Python using Real Python/Automate the Boring Stuff/ and Colt Steele's tutorial

---
+++ automate/Untitled.py
@@ -0,0 +1,22 @@
+#print(2 + 2)
+
+#print(2 + 3 * 6)
+#print((2 + 3) * 6)
+
+#print(48565878 * 5784530)
+
+#exponent
+#print(2 ** 8)
+
+#print(23 / 7)
+
+#Integer division/floored quotient
+#print(23 // 7)
+
+#print(23 % 7)
+
+#print((5 - 1) * (7 + 1) / (3 - 1))
+
+#floats are numbers with a decimal point
+
+#concatenation
---
+++ realPython/chapter1.py
@@ -0,0 +1,40 @@
+print("Hello, World")
+
+phrase = "Hi there"
+print(phrase)
+
+# Use hash for short comments
+
+'''
+You can use 3 single  or 3 double quotes  for multi line comments
+It  works
+hmmmmm
+'''
+
+long_string = '''This is a
+string that spans
+multiple lines.
+                    It also preserves whitespace'''
+print(long_string)
+
+another_long_string = "You can use a \ when you have a lot of text but want on same line"
+print(another_long_string)
+
+my_string = "abc"
+string_length = len(my_string)
+print(string_length)
+
+str1 = "abra"
+str2 = "cadabra"
+print(str1 + " " + str2)
+
+print("abra", "ca", "dabra")
+
+flavor = "birthday cake"
+print(flavor[3])
+
+print(flavor[0:3])
+
+print(flavor[:5])
+
+print(flavor[5:])
